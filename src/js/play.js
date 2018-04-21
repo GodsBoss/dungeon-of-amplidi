d91aa030bce833a8d657d55046bfb38c3f5b1d66
Timo Reitz <godsboss@gmx.de>
2018-04-21 08:36:41 +0200

    Show play UI.

--- src/js/play.js
+++ src/js/play.js
@@ -1,3 +1,41 @@
-class Play extends Phaser.State {}
+class Play extends Phaser.State {
+  create() {
+    for(var x = 0; x < boardSize.width; x++) {
+      for (var y = 0; y < boardSize.height; y++) {
+        this.add.sprite(x * tileSize.width + 20, y * tileSize.width + 20, Math.random() > 0.5 ? 'tile_path' : 'tile_rock')
+      }
+    }
+
+    for(var i = 0; i < maxCards; i++) {
+      this.add.sprite(
+        tileSize.width * boardSize.width * i / maxCards + 20,
+        224,
+        "ui_card"
+      )
+    }
+
+    for(var i = 0; i < maxHeroes; i++) {
+      this.add.sprite(
+        316,
+        tileSize.height * boardSize.height * i / maxHeroes + 20,
+        "ui_heroframe"
+      )
+    }
+  }
+
+  update() {}
+}
 
 export default { State: Play }
+
+// tileSize is the size of a single tile in pixels.
+const tileSize = { width: 12, height: 12 }
+
+// boardSize is the size of the board in tiles.
+const boardSize = { width: 24, height: 16 }
+
+// maxCards is the number of cards the player may have.
+const maxCards = 5
+
+// maxHeroes is the number of heroes the player has.
+const maxHeroes = 4
--- src/js/preload.js
+++ src/js/preload.js
@@ -2,7 +2,19 @@
 * Loads all assets, then moves on to title / level select.
 */
 class Preload extends Phaser.State {
-  preload() {}
+  preload() {
+    [
+      "screen_levelselect",
+      "screen_play",
+      "sprite_dungeonheart",
+      "tile_path",
+      "tile_rock",
+      "ui_card",
+      "ui_heroframe"
+    ].forEach(
+      (key) => this.load.image(key, "gfx/" + key + ".png")
+    )
+  }
 
   create() {
     this.state.start("levelselect")
