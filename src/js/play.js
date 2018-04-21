3627b89f6561b0704b915199ba1192ec86ee863e
Timo Reitz <godsboss@gmx.de>
2018-04-21 09:48:54 +0200

    Introduce Board class.

--- src/js/play.js
+++ src/js/play.js
@@ -1,12 +1,6 @@
 class Play extends Phaser.State {
   create() {
-    const tiles = this.add.group()
-    tiles.classType = Tile
-    for(var x = 0; x < boardSize.width; x++) {
-      for (var y = 0; y < boardSize.height; y++) {
-        tiles.create(x * tileSize.width + 20, y * tileSize.width + 20, Math.random() > 0.5 ? 'tile_path' : 'tile_rock')
-      }
-    }
+    this.board = new Board(this)
 
     for(var i = 0; i < maxCards; i++) {
       this.add.sprite(
@@ -41,6 +35,39 @@ const maxCards = 5
 
 // maxHeroes is the number of heroes the player has.
 const maxHeroes = 4
+
+class Board {
+  constructor(state) {
+    this.tilesGroup = state.add.group()
+    this.tilesGroup.classType = Tile
+    this.tiles = []
+
+    for(var x = 0; x < boardSize.width; x++) {
+      for (var y = 0; y < boardSize.height; y++) {
+        this.addTile(x, y, 'tile_unset')
+      }
+    }
+
+    for(var ix = 0; ix < 4; ix++) {
+      for(var y = Math.floor(boardSize.height / 2) - 1; y <= Math.floor(boardSize.height / 2); y++) {
+        this.setTile([0, 1, boardSize.width-2, boardSize.width-1][ix], y, 'tile_path')
+      }
+    }
+  }
+
+  tileIndex (x, y) {
+    return x + y * boardSize.width
+  }
+
+  setTile (x, y, key) {
+    this.tiles[this.tileIndex(x, y)].destroy()
+    this.addTile(x, y, key)
+  }
+
+  addTile(x, y, key) {
+    this.tiles[this.tileIndex(x, y)] = this.tilesGroup.create(x * tileSize.width + 20, y * tileSize.width + 20, key)
+  }
+}
 
 class Tile extends Phaser.Sprite {
   constructor(game, x, y, key, frame) {
