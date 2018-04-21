fefa59d4b148e60a003cd952b20d771d4315ba47
Timo Reitz <godsboss@gmx.de>
2018-04-21 07:24:58 +0200

    Comment canvas smoothing, seems broken.

--- src/js/bootstrap.js
+++ src/js/bootstrap.js
@@ -5,7 +5,7 @@ class Bootstrap extends Phaser.State {
   preload() {
     Phaser.Canvas.setImageRenderingCrisp(this.game.canvas); // For compatible browsers
     PIXI.scaleModes.DEFAULT = PIXI.scaleModes.NEAREST; // For WebGL
-    Phaser.Canvas.setSmoothingEnabled(this.game.context, false); // For 2D canvas
+    // Phaser.Canvas.setSmoothingEnabled(this.game.context, false); // For 2D canvas
     this.game.scale.scaleMode = Phaser.ScaleManager.USER_SCALE;
     this.game.scale.setUserScale(2, 2);
   }
