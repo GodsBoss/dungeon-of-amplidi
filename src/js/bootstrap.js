483a5ddfba052670c16f0ecd8d08db06bcc6d0d9
Timo Reitz <godsboss@gmx.de>
2018-04-21 06:44:46 +0200

    Set scaling.

--- src/js/bootstrap.js
+++ src/js/bootstrap.js
@@ -2,7 +2,13 @@
 * First state to run, loads just enough to start preload state.
 */
 class Bootstrap extends Phaser.State {
-  preload() {}
+  preload() {
+    Phaser.Canvas.setImageRenderingCrisp(this.game.canvas); // For compatible browsers
+    PIXI.scaleModes.DEFAULT = PIXI.scaleModes.NEAREST; // For WebGL
+    Phaser.Canvas.setSmoothingEnabled(this.game.context, false); // For 2D canvas
+    this.game.scale.scaleMode = Phaser.ScaleManager.USER_SCALE;
+    this.game.scale.setUserScale(2, 2);
+  }
 
   create() {
     this.state.start('preload');
