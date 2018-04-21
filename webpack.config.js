263f7178ad8907093c93bef7e56bd3b5fd83b3ad
Timo Reitz <godsboss@gmx.de>
2018-04-21 04:42:55 +0200

    Create HTML page.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
 const config = {
   "mode": "production",
-  "entry": "./src/init.js"
+  "entry": "./src/init.js",
+  plugins: [new HtmlWebpackPlugin()]
 }
 
 module.exports = config
