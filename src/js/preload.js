3810307ca346de55680346a311b92b4db7265822
Timo Reitz <godsboss@gmx.de>
2018-04-21 08:45:54 +0200

    Add portraits for dead heroes and for no hero.

--- src/js/preload.js
+++ src/js/preload.js
@@ -10,7 +10,9 @@ class Preload extends Phaser.State {
       "tile_path",
       "tile_rock",
       "ui_card",
-      "ui_heroframe"
+      "ui_heroframe",
+      "ui_hero_dead",
+      "ui_hero_none"
     ].forEach(
       (key) => this.load.image(key, "gfx/" + key + ".png")
     )
