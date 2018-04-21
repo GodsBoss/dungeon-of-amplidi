3f61cfd233a017d8d9f23191d6661858de12a728
Timo Reitz <godsboss@gmx.de>
2018-04-21 04:48:07 +0200

    Explicitly set output.

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,15 @@
+const path = require('path')
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const config = {
   "mode": "production",
   "entry": "./src/init.js",
+
+  "output": {
+    filename: "main.js",
+    path: path.resolve(__dirname, 'dist')
+  },
+
   plugins: [
     new HtmlWebpackPlugin(
       {
