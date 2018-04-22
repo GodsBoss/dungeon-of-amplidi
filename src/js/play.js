06a42f5684ae862f3af5c2be87926f53c29554fe
Timo Reitz <godsboss@gmx.de>
2018-04-22 09:14:32 +0200

    Extract Board and Tile into seperate file.

--- src/js/play.js
+++ src/js/play.js
@@ -2,9 +2,11 @@ import array from './array'
 import random from './random'
 import v from './vector'
 
+import board from './play/board'
+
 class Play extends Phaser.State {
   create() {
-    this.board = new Board(this, boardSize, this.overlayClick.bind(this))
+    this.board = board.New(this, board.size, this.overlayClick.bind(this))
     this.party = new Party(this, maxHeroes)
     this.cards = new Cards(this)
     this.heart = new DungeonHeart(this)
@@ -28,136 +30,11 @@ class Play extends Phaser.State {
 
 export default { State: Play }
 
-// tileSize is the size of a single tile in pixels.
-const tileSize = { width: 12, height: 12 }
-
-// boardSize is the size of the board in tiles.
-const boardSize = { width: 24, height: 16 }
-
 // maxCards is the number of cards the player may have.
 const maxCards = 5
 
 // maxHeroes is the number of heroes the player has.
 const maxHeroes = 4
-
-class Board {
-  constructor(state, size, onOverlayClick) {
-    this.size = size
-    this.tilesGroup = state.add.group()
-    this.tilesGroup.classType = Tile
-    this.tiles = []
-    this.overlays = []
-
-    for(var x = 0; x < this.size.width; x++) {
-      for (var y = 0; y < this.size.height; y++) {
-        this.addTile(x, y, 'tile_unset')
-      }
-    }
-
-    for (var x = 0; x < this.size.width; x++) {
-      for (var y = 0; y < this.size.height; y++) {
-        var overlayPos = this.position(x, y)
-        var overlaySprite = state.add.sprite(overlayPos.x, overlayPos.y, 'tile_overlay')
-        overlaySprite.inputEnabled = true
-        overlaySprite.events.onInputUp.add(onOverlayClick)
-        this.overlays[this.tileIndex(x, y)] = overlaySprite
-      }
-    }
-
-    for(var ix = 0; ix < 4; ix++) {
-      for(var y = Math.floor(this.size.height / 2) - 1; y <= Math.floor(this.size.height / 2); y++) {
-        this.setTile([0, 1, this.size.width-2, this.size.width-1][ix], y, 'tile_path')
-      }
-    }
-  }
-
-  tileIndex (x, y) {
-    return x + y * this.size.width
-  }
-
-  getTile (x, y) {
-    return this.tiles[this.tileIndex(x, y)]
-  }
-
-  setTile (x, y, key) {
-    this.tiles[this.tileIndex(x, y)].destroy()
-    this.addTile(x, y, key)
-  }
-
-  addTile(x, y, key) {
-    const pos = this.position(x, y)
-    this.tiles[this.tileIndex(x, y)] = this.tilesGroup.create(pos.x, pos.y, key)
-  }
-
-  inside (x, y) {
-    return x >= 0 && y >= 0 && x < this.size.width && y < this.size.height
-  }
-
-  position (x, y) {
-    return { x: x * tileSize.width + 20, y: y * tileSize.height + 20 }
-  }
-
-  /**
-  * fromPointer takes a pointer and returns the corresponding grid position on the board. May return out-of-bounds position.
-  */
-  fromPointer (ptr) {
-    return {
-      x: Math.floor((ptr.x - 20) / tileSize.width),
-      y: Math.floor((ptr.y - 20) / tileSize.height)
-    }
-  }
-
-  getOverlay (x, y) {
-    return this.overlays[this.tileIndex(x, y)]
-  }
-
-  overlay (offsets) {
-    this.overlays.forEach(
-      (overlay) => overlay.frame = 0
-    )
-    offsets.offsets.forEach(
-      (offset) => {
-        if (this.inside(offset.x, offset.y)) {
-          var overlay = this.getOverlay(offset.x, offset.y)
-          if (!offset.valid) {
-            overlay.frame = 3
-          } else if (offsets.areAllValid()) {
-            overlay.frame = 1
-          } else {
-            overlay.frame = 2
-          }
-        }
-      }
-    )
-  }
-
-  findPassableTiles (currentTile) {
-    return [
-      { x: -1, y: 0},
-      { x: 1, y: 0},
-      { x: 0, y: -1},
-      { x: 0, y: 1}
-    ].map(
-      (offset) => v.add(offset, currentTile)
-    ).filter(
-      (position) => this.inside(position.x, position.y) && this.getTile(position.x, position.y).isPassable()
-    )
-  }
-}
-
-class Tile extends Phaser.Sprite {
-  constructor(game, x, y, key, frame) {
-    super(game, x, y, key, frame)
-  }
-
-  isUnset () {
-    return this.key == "tile_unset"
-  }
-
-  isPassable() {
-    return this.key == "tile_path"
-  }
-}
 
 /**
 * Party is a group of heroes. A party itself has a position, while the heroes gravitate around it.
@@ -171,7 +48,7 @@ class Party {
   constructor (state, maxHeroes) {
     this.phaserState = state
     this.heroes = []
-    this.position = { x: 1, y: Math.floor(boardSize.height / 2) }
+    this.position = { x: 1, y: Math.floor(board.size.height / 2) }
     this.target = this.position
     this.heroes[0] = new Hero(state, this, 0, "knight")
     this.heroes[1] = new Hero(state, this, 1, "cleric")
@@ -231,7 +108,7 @@ class Hero {
   portraitPosition () {
     return {
       x: 316,
-      y: tileSize.height * boardSize.height * this.index / maxHeroes + 20
+      y: board.tileSize.height * board.size.height * this.index / maxHeroes + 20
     }
   }
 
@@ -243,8 +120,8 @@ class Hero {
   * Sets the hero position, (x, y) is a board position, not pixel coordinates.
   */
   setPosition (x, y) {
-    this.heroSprite.x = x * tileSize.width + 20 - 6
-    this.heroSprite.y = y * tileSize.height + 20 - 6
+    this.heroSprite.x = x * board.tileSize.width + 20 - 6
+    this.heroSprite.y = y * board.tileSize.height + 20 - 6
     this.position = { x, y }
   }
 
@@ -516,7 +393,7 @@ class CardSlot {
   }
 
   x () {
-    return tileSize.width * boardSize.width * this.index / maxCards + 20
+    return board.tileSize.width * board.size.width * this.index / maxCards + 20
   }
 
   y () {
@@ -602,8 +479,8 @@ class BoardCard extends Card {
 class BoardCardTile {
   constructor (state, slot, type, offset, maxOffset) {
     this.offset = offset
-    var x = slot.x() + tileSize.width * (offset.x - maxOffset.x / 2) + 20
-    var y = slot.y() + tileSize.height * (offset.y - maxOffset.y / 2) + 24
+    var x = slot.x() + board.tileSize.width * (offset.x - maxOffset.x / 2) + 20
+    var y = slot.y() + board.tileSize.height * (offset.y - maxOffset.y / 2) + 24
     this.tile = state.add.sprite(x, y, "tile_" + type)
   }
 }
---
+++ src/js/play/board.js
@@ -0,0 +1,133 @@
+import v from '../vector'
+
+// tileSize is the size of a single tile in pixels.
+const tileSize = { width: 12, height: 12 }
+
+// boardSize is the size of the board in tiles.
+const boardSize = { width: 24, height: 16 }
+
+class Board {
+  constructor(state, size, onOverlayClick) {
+    this.size = size
+    this.tilesGroup = state.add.group()
+    this.tilesGroup.classType = Tile
+    this.tiles = []
+    this.overlays = []
+
+    for(var x = 0; x < this.size.width; x++) {
+      for (var y = 0; y < this.size.height; y++) {
+        this.addTile(x, y, 'tile_unset')
+      }
+    }
+
+    for (var x = 0; x < this.size.width; x++) {
+      for (var y = 0; y < this.size.height; y++) {
+        var overlayPos = this.position(x, y)
+        var overlaySprite = state.add.sprite(overlayPos.x, overlayPos.y, 'tile_overlay')
+        overlaySprite.inputEnabled = true
+        overlaySprite.events.onInputUp.add(onOverlayClick)
+        this.overlays[this.tileIndex(x, y)] = overlaySprite
+      }
+    }
+
+    for(var ix = 0; ix < 4; ix++) {
+      for(var y = Math.floor(this.size.height / 2) - 1; y <= Math.floor(this.size.height / 2); y++) {
+        this.setTile([0, 1, this.size.width-2, this.size.width-1][ix], y, 'tile_path')
+      }
+    }
+  }
+
+  tileIndex (x, y) {
+    return x + y * this.size.width
+  }
+
+  getTile (x, y) {
+    return this.tiles[this.tileIndex(x, y)]
+  }
+
+  setTile (x, y, key) {
+    this.tiles[this.tileIndex(x, y)].destroy()
+    this.addTile(x, y, key)
+  }
+
+  addTile(x, y, key) {
+    const pos = this.position(x, y)
+    this.tiles[this.tileIndex(x, y)] = this.tilesGroup.create(pos.x, pos.y, key)
+  }
+
+  inside (x, y) {
+    return x >= 0 && y >= 0 && x < this.size.width && y < this.size.height
+  }
+
+  position (x, y) {
+    return { x: x * tileSize.width + 20, y: y * tileSize.height + 20 }
+  }
+
+  /**
+  * fromPointer takes a pointer and returns the corresponding grid position on the board. May return out-of-bounds position.
+  */
+  fromPointer (ptr) {
+    return {
+      x: Math.floor((ptr.x - 20) / tileSize.width),
+      y: Math.floor((ptr.y - 20) / tileSize.height)
+    }
+  }
+
+  getOverlay (x, y) {
+    return this.overlays[this.tileIndex(x, y)]
+  }
+
+  overlay (offsets) {
+    this.overlays.forEach(
+      (overlay) => overlay.frame = 0
+    )
+    offsets.offsets.forEach(
+      (offset) => {
+        if (this.inside(offset.x, offset.y)) {
+          var overlay = this.getOverlay(offset.x, offset.y)
+          if (!offset.valid) {
+            overlay.frame = 3
+          } else if (offsets.areAllValid()) {
+            overlay.frame = 1
+          } else {
+            overlay.frame = 2
+          }
+        }
+      }
+    )
+  }
+
+  findPassableTiles (currentTile) {
+    return [
+      { x: -1, y: 0},
+      { x: 1, y: 0},
+      { x: 0, y: -1},
+      { x: 0, y: 1}
+    ].map(
+      (offset) => v.add(offset, currentTile)
+    ).filter(
+      (position) => this.inside(position.x, position.y) && this.getTile(position.x, position.y).isPassable()
+    )
+  }
+}
+
+class Tile extends Phaser.Sprite {
+  constructor(game, x, y, key, frame) {
+    super(game, x, y, key, frame)
+  }
+
+  isUnset () {
+    return this.key == "tile_unset"
+  }
+
+  isPassable() {
+    return this.key == "tile_path"
+  }
+}
+
+export default {
+  New: (state, size, onOverlayClick) => new Board(state, size, onOverlayClick),
+  size: boardSize,
+  Tile,
+  tileSize
+}
