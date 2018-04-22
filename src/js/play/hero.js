b2c2b9d0618916923775c248f1ccbdad6e76a2a2
Timo Reitz <godsboss@gmx.de>
2018-04-22 10:39:12 +0200

    Let hero use behaviour module.

--- src/js/play/hero.js
+++ src/js/play/hero.js
@@ -1,3 +1,4 @@
+import behaviour from './behaviour'
 import board from './board'
 import life from './life'
 import random from '../random'
@@ -19,7 +20,6 @@ class Party {
     this.phaserState = state
     this.heroes = []
     this.currentPosition = { x: 1, y: Math.floor(board.size.height / 2) }
-    this.target = this.currentPosition
     this.heroes[0] = new Hero(state, this, 0, "knight")
     this.heroes[1] = new Hero(state, this, 1, "cleric")
     this.heroes[2] = new Hero(state, this, 2, "none")
@@ -27,35 +27,19 @@ class Party {
     this.heroes.forEach(
       (hero) => hero.setPosition(this.currentPosition.x, this.currentPosition.y)
     )
+    this.behaviour = new behaviour.PursueTarget(
+      this,
+      behaviour.randomTarget(
+        (entity, currentTarget) => entity.phaserState.board.findPassableTiles(currentTarget ? currentTarget : entity.position())
+      )
+    )
   }
 
   update() {
     this.heroes.forEach(
       (hero) => hero.update()
     )
-    if (v.distance(this.currentPosition, this.target) <= this.speed()) {
-      this.findNextTarget()
-    } else {
-      const d = v.diff(this.currentPosition, this.target)
-      const l = v.length(d)
-      this.setPosition(
-        v.add(
-          this.currentPosition,
-          {
-            x: this.speed() * d.x / l,
-            y: this.speed() * d.y / l
-          }
-        )
-      )
-    }
-  }
-
-  findNextTarget() {
-    var targets = this.phaserState.board.findPassableTiles(this.target)
-    if (targets.length > 0) {
-      var index = random.int(0, targets.length - 1)
-      this.target = targets[index]
-    }
+    this.behaviour.behave()
   }
 
   position () {
